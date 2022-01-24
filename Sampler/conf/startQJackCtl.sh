e8994c2c1f3f858fcb5f01fc493945078337dd68
Axel Boudier <axel.boudier@etudiant.univ-lr.fr>
2022-01-24 11:05:59 +0100

    Replace startQJackCtl.sh

--- Sampler/conf/startQJackCtl.sh
+++ Sampler/conf/startQJackCtl.sh
@@ -8,9 +8,15 @@
 
 # Connexion de l'appareil MIDI (port de sortie) au client d'écriture (port d'entrée) Midi Through
 
-aconnect 24:MPD218 14:MIDI Through 
+#selon la valeur de hdmi hotplug dans le fichier /boot/config.txt 
+#les valeurs des canaux de Qjack changent
+
+
+aconnect 28:MPD218 14:MIDI Through #hdmi_force_hotplug = 1
+aconnect 24:MPD218 14:MIDI Through #normal
+aconnect 20:MPD218 14:MIDI Through #hdmi_ignore_hotplug = 1
 
 # Lancement du programme python 
-cd /home/pi/Desktop/2020-2021-INFO2-PTS3-Sampler/src/
+cd ../src/
 
 python Main.py
